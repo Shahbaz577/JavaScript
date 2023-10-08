f6db2450fea9429cfaadb9b310733bb87668ee5a
SHAHBAZ ALI <113308544+Shahbaz577@users.noreply.github.com>
2023-10-08 17:13:02 +0000

    Array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,31 @@
+// Array
+
+const myArr = [2,3,4,5,6,7,8]
+// console.log(myArr);
+// console.log(myArr[4]);
+
+/* NOTES :- 
+1. JavaScript Arrays are resizable and can contain a mix of diff DataType
+2. Array element cannot be accessed using arbitarary String as indexes (myArr["one"])
+3. Array are zero indexing
+*/
+
+
+// Array Methods
+
+//myArr.push(10)      // Add value on last Index
+//myArr.pop()        // Remove value on last Index
+
+//myArr.unshift(9)      // Add value on first Index
+//myArr.shift()         //Remove value on first Index
+
+//console.log(myArr.includes(3)); 
+//console.log(myArr.indexOf(6));
+
+//const newArr = myArr.join()  //Join convert Numver to String 
+//console.log(myArr);
+//console.log(newArr);
+
+
+//Slice, Splice
+
